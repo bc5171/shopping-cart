c2d4ca6c689166043e45e72521612ab0aced82e2
Ben <bconfer3@gatech.edu>
2017-04-19 22:01:13 -0400

    Started to create/code menu.

--- src/shopping_cart/Controller.java
+++ src/shopping_cart/Controller.java
@@ -1,7 +1,47 @@
 package shopping_cart;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.event.ActionEvent;
+
 /**
  * Created by Ben on 4/19/2017.
  */
-public class Controller {
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Loading user data...");
+    }
+
+    @FXML
+    public void handleExitAction(final ActionEvent event) {
+        System.out.println("exiting");
+        System.exit(0);
+        Platform.exit();
+    }
+
+    @FXML
+    private void handleKeyInput(final InputEvent event)
+    {
+        if (event instanceof KeyEvent)
+        {
+            final KeyEvent keyEvent = (KeyEvent) event;
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.A)
+            {
+                provideAboutFunctionality();
+            }
+        }
+    }
+
+    private void provideAboutFunctionality() {
+        System.out.println("You clicked on About!");
+    }
+
 }
--- src/shopping_cart/Main.java
+++ src/shopping_cart/Main.java
@@ -15,8 +15,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Shopping Cart");
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
