b3b5341b5cb8d563466f917adcd62c4467dccbdd
Ben <bconfer3@gatech.edu>
2017-04-19 20:22:13 -0400

    Fixed folder layouts and created new starting files.

---
+++ src/shopping_cart/Controller.java
@@ -0,0 +1,7 @@
+package shopping_cart;
+
+/**
+ * Created by Ben on 4/19/2017.
+ */
+public class Controller {
+}
--- src/shopping_cart/Main.java
+++ src/shopping_cart/Main.java
@@ -1,4 +1,8 @@
-package sample;
+package shopping_cart;
+
+/**
+ * Created by Ben on 4/19/2017.
+ */
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +14,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
