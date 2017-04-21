6eb7b828ce6c4d68e2e3668fc1b1ab00ee166ad1
Ben <bconfer3@gatech.edu>
2017-04-21 15:46:03 -0400

    GUI is finished for the most part.

--- src/shopping_cart/DataController.java
+++ src/shopping_cart/DataController.java
@@ -1,13 +1,34 @@
 package shopping_cart;
 
+import org.omg.CORBA.SystemException;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ben on 4/21/2017.
  */
 public class DataController {
 
+    ArrayList<String> groceryStore;
+
+    DataController() {
+        groceryStore = new ArrayList<>();
+    }
+
     public boolean saveGroceryStore() {
 
 
+
+        return true;
+    }
+
+    public boolean addCart(String cart) {
+        try {
+            groceryStore.add(cart);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
 
         return true;
     }
